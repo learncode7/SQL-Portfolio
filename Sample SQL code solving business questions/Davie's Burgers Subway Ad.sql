baaee813134b9bb9c13a2b7e95e584ca071a768d
learncode7 <61808280+learncode7@users.noreply.github.com>
2022-06-26 11:20:03 -0400

    Create Davie's Burgers Subway Ad.sql

---
+++ Sample SQL code solving business questions/Davie's Burgers Subway Ad.sql
@@ -0,0 +1,71 @@
+-- The restaurant business has been booming and it is now looking to place a catchy advertisement in the local subway station.
+-- Help them dig into their orders table to see if there is anything interesting in there for a funny tagline!
+
+-- 1
+-- What are the column names?
+SELECT * FROM orders
+LIMIT 10;
+
+-- 2 
+-- How recent is this data?
+
+
+-- 3
+-- Instead of selecting all the columns using *, 
+-- write a query that selects only the special_instructions column.
+
+-- Limit the result to 20 rows.
+SELECT special_instructions FROM orders
+WHERE special_instructions IS NOT NULL
+ORDER BY 1
+LIMIT 20;
+
+SELECT special_instructions FROM orders
+WHERE special_instructions LIKE '%box%';
+
+-- 4 
+-- Can you edit the query so that we are only 
+-- returning the special instructions that are not empty?
+
+
+-- 5
+-- Let’s go even further and sort the instructions 
+-- in alphabetical order (A-Z).
+
+
+-- 6
+-- Let’s search for special instructions that have the word ‘sauce’.
+
+-- Are there any funny or interesting ones? 
+
+
+-- 7
+-- Let’s search for special instructions that have the word ‘door’.
+-- Any funny or interesting ones?
+
+
+-- 8
+-- Let’s search for special instructions that have the word ‘box’.
+-- Any funny or interesting ones?
+
+
+-- 9
+-- Instead of just returning the special instructions, also return their order ids.
+
+-- For more readability:
+-- Rename id as ‘#’
+-- Rename special_instructions as ‘Notes’
+
+SELECT special_instructions AS 'Notes', id AS '#'
+FROM orders
+WHERE special_instructions LIKE '%box%';
+
+
+-- 10
+-- Challenge
+-- They have asked you to query the customer who made the phrase. 
+-- Return the item_name restaurant_id, and user_id for the person created the phrase.
+SELECT item_name, restaurant_id, user_id 
+FROM orders
+WHERE special_instructions = 'Draw a narwhal on the delivery box.';
+
