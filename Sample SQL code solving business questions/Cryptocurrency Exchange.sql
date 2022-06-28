ee03504945e932b995ec924b4c82bd3bb245b90a
learncode7 <61808280+learncode7@users.noreply.github.com>
2022-06-28 14:40:47 -0400

    Create Cryptocurrency Exchange.sql

---
+++ Sample SQL code solving business questions/Cryptocurrency Exchange.sql
@@ -0,0 +1,5 @@
+-- Help them analyze their January ledger data using SQL aggregate functions! You are given the transactions table, which contains both money-in and money-out transactions.
+
+-- Q1: Checking out the whole transactions table:
+SELECT * FROM transactions;
+
